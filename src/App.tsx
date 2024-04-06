675d02da7ecbf836e210d16135c638d126f6f126
allenk <ellenaponomarenko@gmail.com>
2024-04-06 16:26:52 +0300

    little fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { Button } from './component/Button';
 import './App.css';
 
 const App = () => {
-	return (
-		<div  className={cnApp('')}>
+    return (
+        <div  className={cnApp('')}>
 			<h1 className={cnApp('Title')}>8.4 React-хуки</h1>
 			<h2 className={cnApp('Subtitle')}>Клик-полл</h2>
 
